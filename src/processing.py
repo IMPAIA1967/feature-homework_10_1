8dc55f7a9005009a789ff321a00abb1bfa477ea2
Dmitry <dima.soin2014@yandex.ru>
2025-05-31 00:05:05 +0300

    completed function

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,20 @@
-def filter_by_state(data_list, state='EXECUTED'):
+def filter_by_state(data: list[dict], state: str = "EXECUTED") -> list[dict]:
     """
-        Фильтрует список словарей по значению ключа 'state'.
-        :param data_list: Список словарей для фильтрации.
-        :param state: Значение ключа 'state' для фильтрации (по умолчанию 'EXECUTED').
+    Функция фильтрует список словарей, оставляя только те, где ключ state равен заданному значению.
+    data: Список словарей, которые нужно отфильтровать.
+    state: Значение, по которому фильтруем (по умолчанию 'EXECUTED').
     """
+    filtered_list = []
+    for item in data:
+        if item.get('state') == state:
+            filtered_list.append(item)
+    return filtered_list
+
+if __name__ == "__main__":
+    data = [
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+    ]
+    print(filter_by_state(data))
