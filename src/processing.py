564cd3e09ca96491acbbcaaa19946e069571f349
Dmitry <dima.soin2014@yandex.ru>
2025-05-30 23:45:55 +0300

    add func filter_by_state

---
+++ src/processing.py
@@ -0,0 +1,6 @@
+def filter_by_state(data_list, state='EXECUTED'):
+    """
+        Фильтрует список словарей по значению ключа 'state'.
+        :param data_list: Список словарей для фильтрации.
+        :param state: Значение ключа 'state' для фильтрации (по умолчанию 'EXECUTED').
+    """
